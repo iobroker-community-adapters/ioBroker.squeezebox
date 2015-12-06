1a776643d9934ab837c02d760d9b4fa8fa14ba53
Samuel Weibel <samuel.weibel@gmail.com>
2015-12-06 20:01:14 +0100

    Initial version of ioBroker.squeezebox adapter.

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-# ioBroker.squeezebox
-ioBroker adapter for Squeezebox Server (Logitech Media Server)
+![Logo](admin/squeezebox.png)
+# ioBroker Logitech Squeezebox Adapter
+
+Controls a Squeezebox Server a.k.a. Logitech Media Server and its players.
+
+## Install
+
+Install this adapter via ioBroker Admin.
+
+1. Open instance config dialog
+2. Enter the IP address or host name of your Squeezebox Server
+3. Lower the time update interval value if you have enough performance in your system.
+4. Save the configuration
+5. Start the adapter
+
+## Configuration
+### Logitech Media Server Address
+This is the IP address or host name of your Squeezebox Server.
+The server must listen to telnet commands on TCP port 9090 (don't confuse this with the web (HTTP) port which will always be a different one).
+
+### Track time update interval (sec)
+Every N seconds the elapsed time of playing tracks is updated.
+Leave this at 5 seconds if you are not using this for visualization.
+If you need more precision, set it to 2 or 1 seconds.
+
+## States
+
+The adapter automatically connects to the configured Squeezebox Server and creates the following states for each player connected to the Squeezebox Server.
+
+The names of the states are formatted like this: squeezebox.<instance>.<player>.<state>
+- <instance> is the ioBroker adapter instance index (usually "0")
+- <player> is the name you gave to the player when configuring it (spaces are replaced by underscores "_")
+- <state> is described in the following sections
+
+### squeezebox.<instance>.<player>.power
+Boolean, read-write
+- true: player is powered on
+- false: plyer is on stand-by
+
+### squeezebox.<instance>.<player>.state
+Enumeration, read-write
+- 0: Pause
+- 1: Play
+- 2: Stop
+
+### squeezebox.<instance>.<player>.volume
+Integer (0...100), read-write
+Playback volume from nothing (0) to maximum (100)
+Be careful when setting high values (>50) as this might hurt your ears (or your loved ones')!
+
+### squeezebox.<instance>.<player>.muting
+Boolean, read-write
+- true: player is muted (playback continues, but loudspeaker is off)
+- false: player is in regular playback mode
+
+### squeezebox.<instance>.<player>.currentTitle
+String, read-only
+The name of the currently playing (or paused) song or stream. Can be empty.
+
+### squeezebox.<instance>.<player>.currentAlbum
+String, read-only
+The name of the album of the currently playing (or paused) song or stream. Can be empty.
+
+### squeezebox.<instance>.<player>.currentArtist
+String, read-only
+The name of the artist of the currently playing (or paused) song or stream. Can be empty.
+
+### squeezebox.<instance>.<player>.currentDuration
+Integer, read-only
+The total length in seconds of the current song or stream.
+
+### squeezebox.<instance>.<player>.currentDurationText
+String, read-only
+The formatted total length of the current song or stream. (Format: "[hh:]mm:ss")
+
+### squeezebox.<instance>.<player>.elapsedTime
+Integer, read-only
+The number of seconds the current song or stream has been played already. This value is updated every "Track time update interval" (see Configuration above)
+
+### squeezebox.<instance>.<player>.elapsedTimeText
+String, read-only
+The formatted time the current song or stream has been played already. This value is updated every "Track time update interval" (see Configuration above)
+
+## Changelog
+### 0.0.1
+* (UncleSamSwiss) Initial version
+
+## Roadmap/Todo
+
+## License
+
+Apache 2.0
+
+Copyright (c) 2015 UncleSamSwiss
---
+++ squeezebox.js
@@ -0,0 +1,287 @@
+﻿// always required: utils
+var utils = require(__dirname + '/lib/utils');
+
+// other dependencies:
+var logitechmediaserver = require('logitechmediaserver');
+
+// create the adapter object
+var adapter = utils.adapter('squeezebox');
+
+var squeezeboxServer;
+var devices = {}; // mapping of MAC to device object (which has a reference to the player for that device)
+var currentStates = {}; // mapping of state name to state value (so we don't set the same value multiple times)
+
+// unloading
+adapter.on('unload', function (callback) {
+    if (squeezeboxServer && squeezeboxServer.telnet) {
+        try {
+            // TODO: would be great if this was just squeezeboxServer.stop()
+            squeezeboxServer.telnet.writeln("listen 0");
+            squeezeboxServer.telnet.destroy();
+        }
+        catch (e) {
+            adapter.log.warn("Cannot stop squeezebox server telnet connection:" + e.toString());
+        }
+    }
+
+    callback();
+});
+
+// is called if a subscribed object changes
+/*adapter.on('objectChange', function (id, obj) {
+    // Warning, obj can be null if it was deleted
+    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+});*/
+
+// is called if a subscribed state changes
+adapter.on('stateChange', function (id, state) {
+    if (!id || !state || state.ack || currentStates[id] === state.val) {
+        return;
+    }
+
+    // Warning, state can be null if it was deleted
+    adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
+    var idParts = id.split('.');
+    var dp = idParts.pop();
+    var name = idParts.slice(2).join('.');
+    var device = null;
+    for (var mac in devices) {
+        if (devices[mac].channelName == name) {
+            device = devices[mac];
+            break;
+        }
+    }
+
+    if (!device || !device.player) {
+        return;
+    }
+
+    var player = device.player;
+    var val = state.val;
+    if (dp == 'state') {
+        if (val == 0) {
+            player.runTelnetCmd("pause");
+        }
+        else if (val == 1) {
+            player.switchOn();
+            player.runTelnetCmd("play");
+        }
+        else {
+            player.switchOff();
+        }
+    }
+    else if (dp == 'power') {
+        if (val == 0) {
+            player.switchOff();
+        }
+        else {
+            player.switchOn();
+        }
+    }
+    else if (dp == 'muting') {
+        player.runTelnetCmd(!!val ? 'mixer muting 1' : 'mixer muting 0'); // !! is toBoolean()
+    }
+    else if (dp == 'volume') {
+        player.runTelnetCmd('mixer volume ' + val);
+    }
+});
+
+// startup
+adapter.on('ready', function () {
+    main();
+});
+
+function main() {
+    adapter.subscribeStates('*');
+
+    squeezeboxServer = new logitechmediaserver(adapter.config.server);
+    squeezeboxServer.on("registration_finished", function () {
+
+        adapter.log.info('creating/updating player channels');
+        for (var mac in squeezeboxServer.players) {
+            adapter.log.info("Found player " + mac);
+            var device = {
+                mac: mac,
+                name: null,
+                channelName: null,
+                player: squeezeboxServer.players[mac],
+                duration: 0,
+                elapsed: 0
+            };
+            devices[mac] = device;
+            preparePlayer(device);
+        }
+    });
+
+    squeezeboxServer.start();
+}
+
+function setStateAck(name, value)
+{
+    if (currentStates[name] !== value) {
+        currentStates[name] = value;
+        adapter.setState(name, { val: value, ack: true });
+    }
+}
+
+function toFormattedTime(time) {
+    var hours = Math.floor(time / 3600);
+    hours = (hours) ? (hours + ':') : '';
+    var min = Math.floor(time / 60) % 60;
+    if (min < 10) min = '0' + min;
+    var sec = time % 60;
+    if (sec < 10) sec = '0' + sec;
+    
+    return hours + min + ':' + sec;
+}
+
+function preparePlayer(device) {
+    var player = device.player;
+    
+    player.on('logitech_event', function (data) {
+        adapter.log.info("Got event from " + device.mac + ": " + data);
+        processSqueezeboxEvents(device, data.split(' '));
+    });
+    
+    player.on('volume', function (data) {
+        adapter.log.debug("Got volume from " + device.mac + ": " + data);
+        if (device.channelName !== null) {
+            setStateAck(device.channelName + '.volume', player.volume);
+        }
+    });
+    
+    player.on('signalstrength', function (data) {
+        adapter.log.debug("Got signal strength from " + device.mac + ": " + data);
+    });
+    
+    player.on('power', function (data) {
+        adapter.log.debug("Got power from " + device.mac + ": " + data);
+        if (device.channelName !== null) {
+            setStateAck(device.channelName + '.power', data == '1');
+        }
+    });
+    
+    player.on('mode', function (data) {
+        adapter.log.debug("Got mode from " + device.mac + ": " + data);
+        if (device.channelName === null) {
+            return;
+        }
+
+        if (data == 'play') {
+            setStateAck(device.channelName + '.state', 1); // 1 = play
+            player.runTelnetCmd('duration ?');
+            if (!device.elapsedTimer) {
+                player.runTelnetCmd('time ?');
+            }
+            return;
+        }
+        
+        if (device.elapsedTimer) {
+            clearInterval(device.elapsedTimer);
+            device.elapsedTimer = null;
+        }
+        
+        if (data == 'pause') {
+            setStateAck(device.channelName + '.state', 0); // 0 = pause
+        }
+        else {
+            setStateAck(device.channelName + '.state', 2); // 2 = stop
+        }
+    });
+    
+    player.on('name', function (data) {
+        adapter.log.debug("Got name from " + device.mac + ": " + data);
+        if (device.name === null) {
+            device.name = data;
+            completePlayer(device);
+        }
+    });
+    
+    player.on('current_title', function (data) {
+        adapter.log.debug("Got current_title from " + device.mac + ": " + data);
+        setStateAck(device.channelName + '.currentTitle', data);
+    });
+}
+
+var channelNames = [];
+function completePlayer(device) {
+    var channelName = device.name.trim().replace(/\s/g, '_');
+    adapter.log.debug('Player channel of ' + mac + ' is called ' + channelName);
+    if (channelNames.indexOf(channelName) !== -1) {
+        adapter.log.warn('channel "' + channelName + '" already exists, skipping player');
+        return;
+    }
+
+    channelNames.push(channelName);
+    device.channelName = channelName;
+    
+    // request all information we need
+    device.player.runTelnetCmd('mixer muting ?');
+    device.player.runTelnetCmd("current_title ?");
+    device.player.runTelnetCmd("artist ?");
+    device.player.runTelnetCmd("album ?");
+    device.player.runTelnetCmd("mode ?");
+}
+
+function processSqueezeboxEvents(device, eventData) {
+    if (eventData.length == 0)
+        return;
+    
+    if (eventData[0] == 'playlist') {
+        device.player.runTelnetCmd("current_title ?");
+        device.player.runTelnetCmd("artist ?");
+        device.player.runTelnetCmd("album ?");
+        device.player.runTelnetCmd("mode ?");
+        
+        if (eventData[1] == 'newsong') {
+            if (device.elapsedTimer) {
+                clearInterval(device.elapsedTimer);
+                device.elapsedTimer = null;
+                device.player.runTelnetCmd('time ?');
+            }
+        }
+        return;
+    }
+    
+    if (eventData[0] == 'artist') {
+        eventData.shift();
+        setStateAck(device.channelName + '.currentArtist', eventData.join(' '));
+        return;
+    }
+    
+    if (eventData[0] == 'album') {
+        eventData.shift();
+        setStateAck(device.channelName + '.currentAlbum', eventData.join(' '));
+        return;
+    }
+    
+    if (eventData[0] == 'mixer' && eventData[1] == 'muting') {
+        setStateAck(device.channelName + '.muting', eventData.length > 2 && eventData[2] == '1');
+        return;
+    }
+    
+    if (eventData[0] == 'duration') {
+        var duration = Math.floor(parseFloat(eventData[1]));
+        setStateAck(device.channelName + '.currentDuration', duration);
+        setStateAck(device.channelName + '.currentDurationText', toFormattedTime(duration));
+        device.duration = duration;
+        return;
+    }
+    
+    if (eventData[0] == 'time') {
+        // start the elapsedTimer to update the ELAPSED_TIME and ELAPSED_TIME_S
+        if (!device.elapsedTimer) {
+            device.elapsed = Math.floor(parseFloat(eventData[1]));
+            setStateAck(device.channelName + '.elapsedTime', device.elapsed);
+            setStateAck(device.channelName + '.elapsedTimeText', toFormattedTime(device.elapsed));
+            var interval = parseInt(adapter.config.elapsedInterval || 5);
+            device.elapsedTimer = setInterval(function (mac_) {
+                device.elapsed += interval;
+                
+                setStateAck(device.channelName + '.elapsedTime', device.elapsed);
+                setStateAck(device.channelName + '.elapsedTimeText', toFormattedTime(device.elapsed));
+
+            }, interval * 1000, device.uuid);
+        }
+    }
+}
